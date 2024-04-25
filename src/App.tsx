45f958b6487f49827dc7ee8a41e612555880bb1f
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2024-04-25 17:03:43 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,41 @@ function App() {
     name: "",
     website:""
   }]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [status, setStatus] = useState(false);
+  
   const url = 'https://jsonplaceholder.typicode.com/users';
   async function getUsers() {
+    setIsLoading(true);
     const response = await fetch(url);
     const users = await response.json();
-    console.log(users);
+    if (response.status > 300) {
+      setIsLoading(false);
+      setStatus(true);
+    }
+    console.log(users); 
+    
     setUser(users);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getUsers();
-  },[])
+  }, []);
+
+  if (isLoading) return <h1>Loading......</h1>
+  if (status) return <h1>server error 404 </h1>
 
   return (
     <>
       <h1>Github User</h1>
-      <div>
+      <div className='container'>
         {user.map(item => {
           return (
-            <ul key={item.id}>
+            <ul key={item.id} style={{ listStyle: "none", background: "#ffff",margin:"1rem",padding:"10px" }}>
               <li><img src='https://rb.gy/88nfvv' /></li>
               <li>{item.name}</li>
-              <li><a href={item.website}>Profile</a></li>
+              <li><a href={`https://www.${item.website}`}>Profile</a></li>
             </ul>
           )
         })}
